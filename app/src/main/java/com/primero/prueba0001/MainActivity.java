2b1b057039c1fd8bdcbfc41d92d711a542c497b7
andxz <andxxz@gmail.com>
2022-04-20 20:11:31 -0400

    V001 20-04-2022

--- app/src/main/java/com/primero/prueba0001/MainActivity.java
+++ app/src/main/java/com/primero/prueba0001/MainActivity.java
@@ -3,12 +3,34 @@ package com.primero.prueba0001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //TextView
+    EditText etNombreM,etApellidoM;
+    TextView tvResultadoM;
+    Button btAceptarM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etNombreM=(EditText) findViewById(R.id.etNombre);
+        etApellidoM=(EditText) findViewById(R.id.etApellido);
+        etApellidoM.setText("");
+        etNombreM.setText("");
+        tvResultadoM=(TextView) findViewById(R.id.tvResultado);
+        btAceptarM=(Button) findViewById(R.id.btAceptar);
+
+        btAceptarM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvResultadoM.setText(etNombreM.getText() + " " + etApellidoM.getText());
+            }
+        });
     }
 }
